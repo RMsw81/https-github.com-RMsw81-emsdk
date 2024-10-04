6c7e6f59082177ad839de7dfa7bdb26d187a4820
RMsw81 <roberta.merlo81@gmail.com>
2024-10-04 14:42:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -230,6 +230,3 @@ def logout():
 @login_required
 def serve_file(filename):
     return send_from_directory('p/build/web', filename)
-
-if __name__ == "__main__":
-    os.system("gunicorn -w 4 -b 0.0.0.0:5001 app:app")
